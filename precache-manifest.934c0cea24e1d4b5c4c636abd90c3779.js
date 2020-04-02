fc832564ed637b06b2092cf1333b680d2afaf6b2
mansmahamat <mansour.mahamat.salle@gmail.com>
2020-04-02 11:48:52 +0200

    Updates

---
+++ precache-manifest.934c0cea24e1d4b5c4c636abd90c3779.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78d185e775d7d3a4f4eb942c4854417",
+    "url": "/index.html"
+  },
+  {
+    "revision": "957ae2655a87c5c4859f",
+    "url": "/static/css/main.b811251e.chunk.css"
+  },
+  {
+    "revision": "6854d8252fa105355021",
+    "url": "/static/js/2.c6f3d00a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c6f3d00a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "957ae2655a87c5c4859f",
+    "url": "/static/js/main.a8977cd9.chunk.js"
+  },
+  {
+    "revision": "a20b00b1c849a3238210",
+    "url": "/static/js/runtime-main.2f4271a1.js"
+  },
+  {
+    "revision": "774797ac5ef7d12b8d4d8ddf60d66e0a",
+    "url": "/static/media/bundesliga.774797ac.png"
+  },
+  {
+    "revision": "c3456072d5df818f94c9dd8bfb77db6b",
+    "url": "/static/media/laliga.c3456072.png"
+  },
+  {
+    "revision": "09b84628a9bc63abe8f56db8bcc956f3",
+    "url": "/static/media/newLigue1.09b84628.png"
+  },
+  {
+    "revision": "661ada8610ce37decf058940501f1b30",
+    "url": "/static/media/premierleague.661ada86.png"
+  },
+  {
+    "revision": "63a47c3f56764a7b6d927b17d138b38f",
+    "url": "/static/media/serieA.63a47c3f.png"
+  }
+]);
